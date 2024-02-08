a743fc969eb7fd47358ac1974c7e4a800050cd8e
JakeBuroker <shamrecks@gmail.com>
2024-02-08 10:40:47 -0600

    axios hooked up to front end of details page

--- src/components/Admin/Admin.jsx
+++ src/components/Admin/Admin.jsx
@@ -1,14 +1,31 @@
 
+import { useEffect } from "react";
+import axios from "axios";
 
 
-function Admin(){
+function Admin() {
+
+useEffect(() => {
+    fetchOrders();
+  }, []);
+
+    const fetchOrders = () => {
+        axios
+          .get('/api/orders')
+          .then((response) => {
+            console.log('order: ', response.data);
+          })
+          .catch((error) => {
+            console.error(error);
+            alert('Could not fetch orders! It is broken');
+          });
 
 return(
 <h1>Admin</h1>
 
 )
 
-
+    }
 }
 
 
