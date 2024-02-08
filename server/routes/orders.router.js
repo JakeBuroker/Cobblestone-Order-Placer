3a2844e90af6e0b5b2d134b8cd836959abd5dc1f
JakeBuroker <shamrecks@gmail.com>
2024-02-08 14:00:32 -0600

    added routes for deleting and marking orders complete to the backend

--- server/routes/orders.router.js
+++ server/routes/orders.router.js
@@ -52,6 +52,26 @@ router.get('/', (req, res) => {
   });
 })
 
+router.delete('/:id', (req, res) => {
+  pool.query('DELETE FROM "orders" WHERE order_id=$1', [req.params.id]).then((result) => {
+      res.sendStatus(200);
+  }).catch((error) => {
+      console.log('Error DELETE /api/order', error);
+      res.sendStatus(500);
+  })
+});
+
+router.put('/:id', (req, res) => {
+  pool.query(`UPDATE orders
+  SET order_status = 'true'
+  WHERE order_id = $1;
+  `, [req.params.id]).then((result) => {
+      res.sendStatus(200);
+  }).catch((error) => {
+      console.log('Error DELETE /api/order', error);
+      res.sendStatus(500);
+  })
+});
 
 module.exports = router;
 
